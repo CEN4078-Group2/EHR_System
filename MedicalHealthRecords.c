ec29e7d6a6113b165192029b2794d5697f5e46af
nearl002 <nathan.p.earley@gmail.com>
2016-04-06 14:45:27 -0500

    Update MedicalHealthRecords.c
    zero warnings

--- MedicalHealthRecords.c
+++ MedicalHealthRecords.c
@@ -3,23 +3,38 @@
 
 int main(void){
 	
-	int tempUserID = 0;
+	char* tempUserID = (char*)malloc(sizeof(char) * 100);
+	if(tempUserID == NULL){
+		printf("Unable to allocate memory.");
+		exit(1);
+	}
+	memset(tempUserID, 0, sizeof(char));
 	char* tempUserPassword = (char*)malloc(sizeof(char) * 100);
+	if(tempUserPassword == NULL){
+		printf("Unable to allocate memory.");
+		exit(1);
+	}
 	memset(tempUserPassword, 0, sizeof(char));
 	patientRecord* logonUser = (patientRecord*)malloc(sizeof(patientRecord));
+	if(logonUser == NULL){
+		printf("Unable to allocate memory.");
+		exit(1);
+	}
 	memset(logonUser, 0, sizeof(patientRecord));
-	printLogon(tempUserID, tempUserPassword , logonUser);
+	printLogon(tempUserID, tempUserPassword , logonUser, sizeof(*tempUserID));
 
 	switch(logonUser->role){
       		case 1 :
 			printf("\nWelcome Admin.\n\n");
-			roleAdmin();
+			roleAdmin(tempUserID, sizeof(*tempUserID));
 			break;
       		case 2 :
 			printf("\nWelcome Doctor.\n\n");
-			roleDoctor();
+			roleDoctor(tempUserID, sizeof(*tempUserID));
 			break;
       		case 3 :
+			printf("\nWelcome Nurse.\n\n");
+			roleNurse(tempUserID, sizeof(*tempUserID));
 			break;
 		case 4 :
 			break;
@@ -28,4 +43,10 @@ int main(void){
 			break;
 
 	}
+	free(tempUserID);
+	tempUserID = NULL;
+	free(tempUserPassword);
+	tempUserPassword = NULL;
+	free(logonUser);
+	logonUser = NULL;
 }

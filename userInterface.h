4f4e8a2819ee0ad531ada874606edfe9dc3e6ac1
nearl002 <nathan.p.earley@gmail.com>
2016-04-11 22:03:13 -0500

    Update userInterface.h

--- userInterface.h
+++ userInterface.h
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include "patient.h"
-
+#define structSize sizeof(patientRecord)
 
 void printLogon(char* tempID,char* password, patientRecord* patientLookUp, size_t len);
 void roleChoice(int userID, patientRecord* patientLookUp);
 void roleAdmin(char* tempID, size_t len);
 void roleDoctor(char* tempID, size_t len);
 void roleNurse(char* tempID, size_t len);
-void rolePatient(void);
+void rolePatient(char* tempID, size_t len, patientRecord* logonUser);
 void createNewPatient(void);
 void fileRead(patientRecord *patientLookUp, size_t len);
 void fileWrite(patientRecord *writeuser);
@@ -15,4 +15,7 @@ void userFillNew(patientRecord *writeUser, size_t len);
 void fReader(patientRecord *readUser, FILE *patientFile);
 void fWriter(patientRecord *writeUser, FILE *patientFile);
 int inputValidation(int userID, char* myString);
+void privilegeEscalate(void);
+void deEscalatePrivilege(void);
+int digitOrNot(char *myString);
 
